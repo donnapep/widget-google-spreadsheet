7c230b237eb92874e8ac0f9702d12ee9d28ca9a2
Donna Peplinskie <donnapep@gmail.com>
2017-03-19 06:13:10 -0400

    Refactor main to handle fetching of Widget params

--- src/widget/main.js
+++ src/widget/main.js
@@ -1,18 +1,73 @@
+/* global gadgets */
+
+/**
+ * External Dependencies
+ */
+import React from "react"
+import ReactDom from "react-dom";
+
+/**
+ * Internal Dependencies
+ */
+import Logger from "../components/widget-common/dist/logger";
+import Spreadsheet from "./components/spreadsheet";
 require( "./css/spreadsheet.css" );
 
-const React = require( "react" ),
-  ReactDOM = require( "react-dom" ),
-  Main = require( "./components/main" );
-
-( function( window, document ) {
+( ( window, document ) => {
   "use strict";
 
   // Disable context menu (right click menu)
-  window.oncontextmenu = function() {
+  window.oncontextmenu = () => {
     return false;
   };
 
-  window.addEventListener( "WebComponentsReady", function() {
-    ReactDOM.render( <Main />, document.getElementById( "mainContainer" ) );
-  } );
+  const id = new gadgets.Prefs().getString( "id" );
+
+  if ( id ) {
+    const rpc = gadgets.rpc;
+
+    rpc.register( `rsparam_set_${ id }`, configure );
+    rpc.call( "", "rsparam_get", null, id, [ "companyId", "displayId", "additionalParams" ] );
+  }
+
+  function configure( names, values ) {
+    if ( !Array.isArray( names ) || !names.length || !Array.isArray( values ) || !values.length ) {
+      return;
+    }
+
+    let companyId = "";
+    let displayId = "";
+
+    if ( "companyId" === names[ 0 ] ) {
+      companyId = values[ 0 ];
+    }
+
+    if ( "displayId" === names[ 1 ] ) {
+      if ( values[ 1 ] ) {
+        displayId = values[ 1 ];
+      } else {
+        displayId = "preview";
+      }
+    }
+
+    Logger.setIds( companyId, displayId );
+
+    let additionalParams = null;
+
+    if ( "additionalParams" === names[ 2 ] ) {
+      additionalParams = JSON.parse( values[ 2 ] );
+
+      setParams( additionalParams );
+    }
+  }
+
+  function setParams( additionalParams ) {
+    const prefs = new gadgets.Prefs(),
+      params = JSON.parse( JSON.stringify( additionalParams ) );
+
+    params.width = prefs.getInt( "rsW" );
+    params.height = prefs.getInt( "rsH" );
+
+    ReactDom.render( <Spreadsheet { ...params } />, document.getElementById( "mainContainer" ) );
+  }
 } )( window, document );
